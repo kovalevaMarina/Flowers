7d1f8cdd99e49a0fc7d8d9de0062522be9e078b1
Marina Kovaleva <marinakovaleva259@gmail.com>
2021-10-06 20:03:34 +0300

    Create slider in popular block

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function () {
+  $('.popular__slider').slick({
+    prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow_left.svg" alt="Arrow left">',
+    nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow_right.svg" alt="Arrow right">',
+  });
+})
